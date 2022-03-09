41f0514fa6d917c164d089d8368e73ca2d481f93
Gamedy19 <92826313+Gamedy19@users.noreply.github.com>
2022-03-09 12:15:38 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: Siempre esta cambiando, no constante !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    text("C U N I F O N", 700, 160)
+    fill("lightblue")
+    text("Pista: Almacena toda la informacion !!", 700,180)
+
+    fill("white")
+    text("E D A T E D O S B  A S", 300, 240 )
+    fill("lightblue")
+    text("Pista: Realiza tareas particulares !!", 300, 260)
+
+}
